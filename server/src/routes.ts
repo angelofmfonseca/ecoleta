356df88cbc09e99e495a13b3f5571b553d028b44
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-06-03 21:01:16 -0300

    post locals

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,12 +9,49 @@ routes.get("/items", async (req, res) => {
 
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${ item.image }`
     }
   })
-
   return res.json(serializedItems)
+})
+
+routes.post("/locals", async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp, 
+    latitude,
+    longitude,
+    city, 
+    uf,
+    items
+  } = req.body;
+
+  const insertedIds = await knex("locals").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp, 
+    latitude,
+    longitude,
+    city, 
+    uf
+  })
+
+  const locals_id = insertedIds[0]
+
+  const itemsLocals = items.map((items_id: number) => {
+    return {
+      items_id,
+      locals_id
+    }
+  })
+  
+  await knex("items_locals").insert(itemsLocals)
+
+  return res.json({ success: true })
 })
 
 export default routes
